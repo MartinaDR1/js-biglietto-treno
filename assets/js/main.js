56f925e6a88bd0c86892e93899e6fdb792bdd2fe
MartinaDR1 <derosemartina933@gmail.com>
2023-03-08 17:49:11 +0100

    changes code

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,32 +7,21 @@ const userAge = Number(prompt('Quanti anni hai?'));
 console.log(userAge);
 
 // Valutare il prezzo del biglietto sulla base ai km (0.21 € al km)
-const priceTotal = Math.abs (km * 0.21);
-const resultPriceTotal = priceTotal.toFixed(2)
-console.log(resultPriceTotal);
+let priceTotal = km * 0.21;
 
 // Applicare sconto del 20% per i minorenni e del 40% per gli over65
-const under18 = Math.abs ((priceTotal * 20) / 100);
-const discountTotalUnder18 = Math.abs (priceTotal - under18);
-const resultUnder18= discountTotalUnder18.toFixed(2);
-console.log(resultUnder18);
 
-// Applicare sconto del 40% per gli over65
-const over65 = Math.abs ((priceTotal * 40) / 100);
-const discountTotalOver65 = Math.abs (priceTotal - over65);
-const resultOver65= discountTotalOver65.toFixed(2);
-console.log(resultOver65);
-
-// Stampare risultato in pagina
 if(userAge < 18){
-    const priceTotalTrain = document.getElementById('priceTrain');
-    priceTotalTrain.innerHTML = `Ciao, il prezzo del tuo biglietto è di: ${resultUnder18} €`
+    const under18 = priceTotal * 20 / 100;
+    priceTotal = priceTotal - under18;
 } else if (userAge >= 65){
-    const priceTotalTrain = document.getElementById('priceTrain');
-    priceTotalTrain.innerHTML = `Ciao, il prezzo del tuo biglietto è di: ${resultOver65} €`
-} else {
-    const priceTotalTrain = document.getElementById('priceTrain');
-    priceTotalTrain.innerHTML = `Ciao, il prezzo del tuo biglietto è di: ${resultPriceTotal} €`
+    const over65 = priceTotal * 40 / 100;
+    priceTotal = priceTotal - over65;
 }
 
+// Stampare risultato in pagina
+let resultPriceTotal = priceTotal.toFixed(2)
+console.log(resultPriceTotal);
+const priceTotalTrain = document.getElementById('priceTrain');
+priceTotalTrain.innerHTML = `Ciao, il prezzo del tuo biglietto è di: ${resultPriceTotal} €`
 
